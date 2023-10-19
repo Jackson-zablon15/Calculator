90d7b31cde866e2ae9dac703c34bc71b069e509a
Jackson Zablon <jacksonzabron15@gmail.com>
2023-10-19 17:36:08 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useState } from "react" 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Button ({value}){
+  const [displayValue,setDisplayValue]=useState('clicked');
+  function handleClick(){
+   setDisplayValue(4)
+      }
+  return(
+    <div className="item" onClick={handleClick} >{value}</div>
+  )
 }
 
-export default App;
+export default function Calculator({displayValue}){
+ 
+  return(
+    <>
+    <h1 style={{textAlign:"center"}}>Simple Calculator</h1>
+    <div className="grid-container">
+    <div className="item1">{displayValue}</div>
+ 
+    <Button value={"C"} />
+    <Button value={"()"}/>
+    <Button value={"%"}/>
+    <Button value={"/"}/>
+
+    <Button value={7}/>
+    <Button value={8}/>
+    <Button value={9}/>
+    <Button value={"X"}/>
+   
+    <Button value={4}/>
+    <Button value={5}/>
+    <Button value={6}/>
+    <Button value={"-"}/>
+
+    <Button value={1}/>
+    <Button value={2}/>
+    <Button value={3}/>
+    <Button value={"+"}/>
+   
+    <Button value={"(-)"}/>
+    <Button value={0}/>
+    <Button value={"."}/>
+    <Button value={"="}/>
+    </div>
+   
+    </>
+  )
+}
